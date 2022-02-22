475bbfaa3255623c117f2a6f2012d4278fdfe771
RafaelEstevamReis <rafaelestevamreis@gmail.com>
2022-02-22 15:48:20 -0300

    Add example

--- DBF.Test/Program.cs
+++ DBF.Test/Program.cs
@@ -1,3 +1,12 @@
 ﻿using System;
 
 Console.WriteLine("Hello World!");
+
+var table = Simple.DBF.Reader.Open("db.dbf", encoding: System.Text.Encoding.UTF8);
+table = table;
+
+var buildModelClass = table.ExportModelClassTemplate();
+
+// Use this string to create a model class
+buildModelClass = buildModelClass;
+
